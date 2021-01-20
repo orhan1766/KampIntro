bbecf2042d2b43df1466791f5a78ecf24cfb5a5f
orhan1766@gmail.com <orhan1766@gmail.com>
2021-01-20 23:51:06 +0300

    20 Ocak ders sonu

---
+++ Koleksiyonlar/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Koleksiyonlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] isimler = new string[] { "Orhan", "Mesut", "Emre", "Fatih" };
+            //Console.WriteLine(isimler[0]);
+            //Console.WriteLine(isimler[1]);
+            //Console.WriteLine(isimler[2]);
+            //Console.WriteLine(isimler[3]);
+
+            List<string> isimler2 = new List<string> { "Orhan", "Mesut", "Emre", "Fatih" };
+
+            foreach (var isim in isimler2)
+            {
+                Console.WriteLine(isim);
+            }
+            Console.WriteLine("-------------");
+
+            isimler2.Add("Behzat");
+
+            foreach (var isim in isimler2)
+            {
+                Console.WriteLine(isim);
+            }
+        }
+    }
+}
