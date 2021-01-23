7cd25d76a2e509bd0f0bc1ca411aa28884f9bfac
orhan1766@gmail.com <orhan1766@gmail.com>
2021-01-24 00:00:39 +0300

    23 Ocak Ders Sonu

---
+++ OOP3/ILoggerService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP3
+{
+    //interfaceler bibirinin alternatifi olabilecek claslar için kullanılabilir.Örn: kredi türleri, loglama türleri
+    interface ILoggerService
+    {
+        void Log();
+    }
+}
